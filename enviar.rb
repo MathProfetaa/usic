02452eb3565e4a234a114e6fd822a0746619b0da
Matheus <matheus.lemes@ritain.io>
2024-02-08 11:27:21 +0000

    aa

--- enviar.rb
+++ enviar.rb
@@ -1,40 +1,33 @@
 require 'net/smtp'
 
-# Obter os dados do formulário
+# Configurações do SMTP
+host = 'smtp.zoho.eu'
+port = 587
+username = 'contato@matheusqa.eu' # Substitua por seu email
+password = '020921M@ria' # Substitua por sua senha
+
+# Dados do email
+remetente = 'contato@matheusqa.eu' # Substitua por seu email
+destinatario = 'contato@matheusqa.eu' # Substitua pelo email de destino
+assunto = 'Contato pelo site'
+
+# Leitura dos dados do formulário
 nome = params[:nome]
 email = params[:email]
 mensagem = params[:mensagem]
 
-# Validar os valores
-if nome.blank? || email.blank? || mensagem.blank?
-  flash[:error] = 'Preencha todos os campos!'
-  redirect_to '/'
-end
-
-# Criar o corpo da mensagem
-corpo_mensagem = <<~MSG
+# Conteúdo do email
+corpo_email = <<~HTML
   Nome: #{nome}
-  E-mail: #{email}
+  Email: #{email}
   Mensagem: #{mensagem}
-MSG
+HTML
 
-# # Configurar o SMTP
-# smtp = Net::SMTP.new('sandbox.smtp.mailtrap.io', 2525)
-# smtp.start('sandbox.smtp.mailtrap.io', 'b777da751d2726', '0df76af114df84', :plain)
-
-# # Enviar o e-mail
-# smtp.send_message("#{mensagem}", "#{email}", 'contato@matheusqa.eu')
-
-# # Fechar a conexão SMTP
-# smtp.finish
-
-
-
-Net::SMTP.start('smtp.zoho.eu', 465, 'smtp.zoho.eu', 'contato@matheusqa.eu', '020921M@ria', :login) do |smtp|
-  smtp.send_message("#{mensagem}", "#{email}", 'contato@matheusqa.eu')
-  smtp.finish
+# Conexão com o servidor SMTP
+smtp = Net::SMTP.new(host, port)
+smtp.start(username, password, :plain) do |smtp|
+  smtp.send_message(corpo_email, remetente, destinatario, assunto)
 end
 
 # Mensagem de sucesso
-flash[:success] = 'Mensagem enviada com sucesso!'
-redirect_to '/'
+puts 'Email enviado com sucesso!'
